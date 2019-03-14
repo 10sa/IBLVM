8490d508f8e26821d4be6c91130d8facd0849c6b
10sa <zenkins26@gmail.com>
2019-03-14 13:37:03 +0900

    Create BitLocker model.

---
+++ IBLVM-Libaray/BitLocker/BitLocker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace IBLVM_Libaray.BitLocker
+{
+	class BitLocker
+	{
+		private ManagementBaseObject bitlockerObject;
+
+		private BitLocker() { }
+	
+		private BitLocker(ManagementBaseObject bitlockerObject)
+		{
+			this.bitlockerObject = bitlockerObject;
+		}
+
+		public static BitLocker[] GetVolumes()
+		{
+			var path = new ManagementPath(@"Root\CIMV2\Security\MicrosoftVolumeEncryption") {
+				ClassName = "Win32_EncryptableVolume"
+			};
+
+			List<BitLocker> collection = new List<BitLocker>();
+			foreach (var volume in new ManagementClass(new ManagementScope(path), path, new ObjectGetOptions()).GetInstances())
+				collection.Add(new BitLocker(volume));
+
+			return collection.ToArray();
+		}
+	}
+}
