cd97862a704e2299b8e9c4f273b3735e3c993036
10sa <zenkins26@gmail.com>
2019-03-14 14:07:38 +0900

    Create BitLocker methods, ProtectionStatus Enum.

--- IBLVM-Libaray/BitLocker/BitLocker.cs
+++ IBLVM-Libaray/BitLocker/BitLocker.cs
@@ -4,20 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Management;
+using System.ComponentModel;
 
 namespace IBLVM_Libaray.BitLocker
 {
 	class BitLocker
 	{
-		private ManagementBaseObject bitlockerObject;
+		private ManagementObject bitlockerObject;
 
 		private BitLocker() { }
 	
-		private BitLocker(ManagementBaseObject bitlockerObject)
+		private BitLocker(ManagementObject bitlockerObject)
 		{
 			this.bitlockerObject = bitlockerObject;
 		}
 
+		#region Private methods
+		private ManagementBaseObject InvokeMethod(string method, params object[] args)
+		{
+			ManagementBaseObject result = (ManagementBaseObject)bitlockerObject.InvokeMethod(method, args);
+			ErrorValidation(result);
+
+			return result;
+		}
+
+		private void ErrorValidation(ManagementBaseObject result)
+		{
+			uint code = (uint)result["returnValue"];
+			if (code == 0x0)
+				throw new Win32Exception(Convert.ToInt32(code));
+		}
+
+		#endregion
+
+		#region Public methods
+		/// <summary>
+		/// BitLocker가 적용된 볼륨을 반환합니다.
+		/// </summary>
+		/// <returns>BitLocker가 적용된 볼륨 목록.</returns>
 		public static BitLocker[] GetVolumes()
 		{
 			var path = new ManagementPath(@"Root\CIMV2\Security\MicrosoftVolumeEncryption") {
@@ -25,10 +49,22 @@ namespace IBLVM_Libaray.BitLocker
 			};
 
 			List<BitLocker> collection = new List<BitLocker>();
-			foreach (var volume in new ManagementClass(new ManagementScope(path), path, new ObjectGetOptions()).GetInstances())
+			foreach (ManagementObject volume in new ManagementClass(new ManagementScope(path), path, new ObjectGetOptions()).GetInstances())
 				collection.Add(new BitLocker(volume));
 
 			return collection.ToArray();
 		}
+
+		public string DeviceID => (string)bitlockerObject["DeviceID"];
+
+		public string DriveLetter => (string)bitlockerObject["DriveLetter"];
+		
+		public ProtectionStatus GetProtectionStatus()
+		{
+			ManagementBaseObject result = InvokeMethod("GetProtectionStatus");
+
+			return (ProtectionStatus)result["ProtectionStatus"];
+		}
+		#endregion
 	}
 }
---
+++ IBLVM-Libaray/BitLocker/ProtectionStatus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBLVM_Libaray.BitLocker
+{
+	enum ProtectionStatus : uint
+	{
+		/// <summary>
+		/// 디스크가 잠겨 있음을 나타냅니다.
+		/// </summary>
+		Locked = 0,
+
+		/// <summary>
+		/// 디스크가 잠금 해제되어 있음을 나타냅니다.
+		/// </summary>
+		Unlocked,
+
+		/// <summary>
+		/// 디스크가 BitLocker으로 보호되고 있지 않음을 나타냅니다.
+		/// </summary>
+		NotBitlockerVolume
+	}
+}
