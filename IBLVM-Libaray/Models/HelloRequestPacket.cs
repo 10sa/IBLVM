de86034690f62c5cc42f11590f6548527292ec5e
10sa <zenkins26@gmail.com>
2019-03-21 13:31:15 +0900

    Create base packets.

---
+++ IBLVM-Libaray/Models/HelloRequestPacket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IBLVM_Libaray.Interfaces;
+
+namespace IBLVM_Libaray.Models
+{
+	class HelloRequestPacket : IPacket
+	{
+		public byte[] GetBytes()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ IBLVM-Libaray/Models/HelloResponsePacket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IBLVM_Libaray.Interfaces;
+
+namespace IBLVM_Libaray.Models
+{
+	class HelloResponsePacket : IPacket
+	{
+		public byte[] GetBytes()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
