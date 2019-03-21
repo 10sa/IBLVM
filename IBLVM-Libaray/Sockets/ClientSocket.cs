f15c732e452cd87bc1422e72984e3873a03797a8
10sa <zenkins26@gmail.com>
2019-03-21 15:02:29 +0900

    Implements protocol handshake.

--- IBLVM-Libaray/Sockets/ClientSocket.cs
+++ IBLVM-Libaray/Sockets/ClientSocket.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+using IBLVM_Libaray.Models;
+using IBLVM_Libaray.Enums;
 
 namespace IBLVM_Libaray.Sockets
 {
-	class ClientSocket
+	public class ClientSocket
 	{
+		private readonly Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+		private readonly byte[] socketBuffer = new byte[256];
+
+		public void Connect(IPEndPoint remoteEndPoint)
+		{
+			socket.Connect(remoteEndPoint);
+			Handshake();
+		}
+
+		private void Handshake()
+		{
+			socket.Send(new HelloRequestPacket().GetPacketBytes());
+			socket.Receive(socketBuffer, 0, BasePacket.GetPacketSize(), SocketFlags.None);
+
+			if (socketBuffer.SequenceEqual(BasePacket.MagicBytes) &&
+					(PacketType)BitConverter.ToUInt16(socketBuffer, BasePacket.MagicBytes.Length) == PacketType.Ack)
+				socket.Send(new HelloResponsePacket().GetPacketBytes());
+			else
+				throw new ProtocolViolationException("Received wrong header.");
+		}
 	}
 }
