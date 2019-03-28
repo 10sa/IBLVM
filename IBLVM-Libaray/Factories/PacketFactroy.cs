59cb90597f12e22c528f73786e03ce8d55552f8e
10sa <zenkins26@gmail.com>
2019-03-28 14:49:49 +0900

    Add packet factory interface, class.

---
+++ IBLVM-Libaray/Factories/PacketFactroy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IBLVM_Libaray.Interfaces;
+using IBLVM_Libaray.Models;
+
+namespace IBLVM_Libaray.Factories
+{
+	public class PacketFactroy : IPacketFactory
+	{
+		public IPacket GetHelloRequest()
+		{
+			return new HelloRequestPacket();
+		}
+
+		public IPacket GetHelloResponse()
+		{
+			return new HelloResponsePacket();
+		}
+	}
+}
