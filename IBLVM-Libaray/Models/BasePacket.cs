1266345b38b1aecbd44c0c8e9499619f77e35332
10sa <zenkins26@gmail.com>
2019-04-10 15:29:45 +0900

    Fix wrong header parsing.

--- IBLVM-Libaray/Models/BasePacket.cs
+++ IBLVM-Libaray/Models/BasePacket.cs
@@ -18,16 +18,25 @@ namespace IBLVM_Libaray.Models
 
 		public static readonly byte[] MagicBytes = new byte[] { 0xDA, 0xAB, 0xBC, 0xCD };
 
+		// This variable is parsed instance's payload size //
+		private readonly int payloadSize = 0;
+
 		private BasePacket() { }
 
 		public BasePacket(byte[] data, ref int offset)
 		{
-			if (!data.SequenceEqual(MagicBytes))
-				throw new ArgumentException("Invalid MagicBytes!");
-
+			for(int i = 0; i < MagicBytes.Length; i++)
+			{
+				if (data[i] != MagicBytes[i])
+					throw new ArgumentException("Invalid MagicBytes!");
+			}
 			offset += MagicBytes.Length;
-			Type = (PacketType)BitConverter.ToUInt16(data, offset);
-			offset += sizeof(uint);
+
+			payloadSize = BitConverter.ToInt32(data, offset);
+			offset += sizeof(int);
+
+			Type = (PacketType)BitConverter.ToInt16(data, offset);
+			offset += sizeof(short);
 		}
 
 		protected BasePacket(PacketType type) => Type = type;
@@ -36,7 +45,7 @@ namespace IBLVM_Libaray.Models
 		{
 			WriteToStream(buffer, MagicBytes);
 			WriteToStream(buffer, BitConverter.GetBytes(GetPayloadSize()));
-			WriteToStream(buffer, BitConverter.GetBytes((ushort)Type));
+			WriteToStream(buffer, BitConverter.GetBytes((short)Type));
 		}
 
 		protected static void WriteToStream(Stream stream, byte[] data)
@@ -53,13 +62,17 @@ namespace IBLVM_Libaray.Models
 			}
 		}
 
-		public virtual int GetPayloadSize() => 0;
+		public virtual int GetPayloadSize() => payloadSize;
 
 		public static int GetHeaderSize() => MagicBytes.Length + sizeof(int) + sizeof(PacketType);
 
 		public virtual Stream GetPayloadStream() => new MemoryStream(256);
 
-		public virtual void ParsePayload(int payloadSize, Stream stream) { }
+		public virtual void ParsePayload(int payloadSize, Stream stream)
+		{
+			// Re-define temp size //
+			payloadSize = 0;
+		}
 
 		int IPacket.GetPayloadSize() => GetPayloadSize();
 	}
