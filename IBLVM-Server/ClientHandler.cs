8033c405605b3d9f3ffe822f26067f0b22754387
10sa <zenkins26@gmail.com>
2019-04-11 08:25:27 +0900

    Rename server class, Add handler chain.

---
+++ IBLVM-Server/ClientHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+
+using CryptoStream;
+
+namespace IBLVM_Server
+{
+	class ClientHandler
+	{
+		public Thread Thread;
+
+		private CryptoMemoryStream cryptoStream;
+		private Socket socket;
+
+		public ClientHandler(Socket socket)
+		{
+			this.socket = socket;
+		}
+
+		public void Start()
+		{
+			Thread = new Thread(() =>
+			{
+
+			});
+		}
+	}
+}
---
+++ IBLVM-Server/IBLVMServer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using System.Net.Sockets;
+using System.Net;
+
+namespace IBLVM_Server
+{
+	class IBLVMServer
+	{
+		public Thread ServerThread { get; private set; }
+
+		private Socket serverSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+		private List<ClientHandler> clientHandlers = new List<ClientHandler>();
+
+		public void Bind(EndPoint localEndPoint) => serverSocket.Bind(localEndPoint);
+
+		public void Listen(int backlog) => serverSocket.Listen(backlog);
+
+		public void Start()
+		{
+			ServerThread = new Thread(() =>
+			{
+				Socket clientSocket = serverSocket.Accept();
+				ClientHandler clientHandler = new ClientHandler(clientSocket);
+				clientHandlers.Add(clientHandler);
+
+				clientHandler.Start();
+			});
+
+			ServerThread.Start();
+		}
+	}
+}
