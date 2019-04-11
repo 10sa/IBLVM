33184449f646a088010007889b8ec66fb07a0546
10sa <zenkins26@gmail.com>
2019-04-11 14:04:10 +0900

    Integrate .NET Framework verison.

--- IBLVM-Tests/ProtocolTests.cs
+++ IBLVM-Tests/ProtocolTests.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System.Net;
+
+using IBLVM_Server;
+using IBLVM_Client;
+
 namespace IBLVM_Tests
 {
 	[TestClass]
@@ -13,7 +18,16 @@ namespace IBLVM_Tests
 		[TestMethod]
 		public void HandshakeTest()
 		{
+			IBLVMServer server = new IBLVMServer();
+			server.Bind(new IPEndPoint(IPAddress.Any, 47857));
+			server.Listen(5);
 
+			server.Start();
+
+			IBLVMClient client = new IBLVMClient();
+			client.Connect(new IPEndPoint(IPAddress.Loopback, 47857));
+
+			client.Dispose();
 		}
 	}
 }
