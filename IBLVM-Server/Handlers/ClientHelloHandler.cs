d467b26bd371bad6db7245f4cbe78afc4131f91a
10sa <zenkins26@gmail.com>
2019-04-11 12:15:19 +0900

    Rename handler, Implement handshake handling.

---
+++ IBLVM-Server/Handlers/ClientHelloHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Security.Cryptography;
+
+using IBLVM_Libaray.Interfaces;
+using IBLVM_Util.Interfaces;
+using IBLVM_Libaray.Models;
+using IBLVM_Util;
+
+using IBLVM_Server.Enums;
+
+using IBLVM_Libaray.Enums;
+
+namespace IBLVM_Server.Handlers
+{
+	class ClientHelloHandler : IPacketHandler
+	{
+		private readonly ECDiffieHellmanCng keyExchanger = new ECDiffieHellmanCng();
+		private IPacketFactory packetFactory;
+
+		public ClientHelloHandler(IPacketFactory packetFactory)
+		{
+			this.packetFactory = packetFactory;
+		}
+
+		public bool Handle(IPacket header, IIBLVMSocket socket)
+		{
+			if (header.Type == PacketType.Hello)
+			{
+				IPacket packet = packetFactory.CreateServerKeyResponse(keyExchanger.PublicKey.ToByteArray());
+				SocketUtil.SendPacket(socket.GetSocketStream(), packet);
+			}
+
+			return false;
+		}
+	}
+}
