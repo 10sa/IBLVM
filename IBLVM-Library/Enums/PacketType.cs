5f2ea8d806a83a4100039153a9d0fc91201420d6
10sa <zenkins26@gmail.com>
2019-06-19 12:08:11 +0900

    Add, Rename, Resorting PackeType enum.

--- IBLVM-Library/Enums/PacketType.cs
+++ IBLVM-Library/Enums/PacketType.cs
@@ -12,21 +12,6 @@ namespace IBLVM_Library.Enums
 	public enum PacketType : short
 	{
 		/// <summary>
-		/// 핸드셰이크 시작을 나타냅니다.
-		/// </summary>
-		Hello = 0,
-
-		/// <summary>
-		/// 핸드셰이크 과정에서 서버측에서 클라이언트에게 보내는 키 응답입니다.
-		/// </summary>
-		ServerKeyResponse,
-
-		/// <summary>
-		/// 핸드셰이크 과정에서 클라이언트에서 서버에게 보내는 키 응답입니다.
-		/// </summary>
-		ClientKeyResponse,
-
-		/// <summary>
 		/// 암호화에 사용되는 초기화 벡터 교환에 대한 요청입니다.
 		/// </summary>
 		IVChangeReqeust,
@@ -37,34 +22,24 @@ namespace IBLVM_Library.Enums
         IVChangeResponse,
 
 		/// <summary>
+		/// 핸드셰이크 시작을 나타냅니다.
+		/// </summary>
+		ClientHello
+
+		/// <summary>
+		/// 핸드셰이크 과정에서 클라이언트에서 서버에게 보내는 키 응답입니다.
+		/// </summary>
+		ClientKeyResponse,
+
+		/// <summary>
 		/// 클라이언트가 서버에 로그인함을 나타냅니다.
 		/// </summary>
 		ClientLoginRequest,
-
-		/// <summary>
-		/// 서버에서 클라이언트 측에 로그인 결과를 알림을 나타냅니다.
-		/// </summary>
-		ServerLoginResponse,
 
         /// <summary>
         /// 서버에서 요청한 BitLocker 볼륨에 대한 응답을 나타냅니다.
         /// </summary>
 		ClientBitLockersResponse,
-
-        /// <summary>
-        /// 서버에서 클라이언트에게 BitLocker 볼륨 목록에 대한 요청을 나타냅니다.
-        /// </summary>
-		ServerBitLockersRequest,
-
-		/// <summary>
-		/// 서버에서 클라이언트의 BitLocker 볼륨의 잠금 해제 명렁을 나타냅니다.
-		/// </summary>
-        ServerBitLockerUnlockCommand,
-
-		/// <summary>
-		/// 서버에서 클라이언트의 BitLocker 볼륨의 잠금 명령을 나타냅니다.
-		/// </summary>
-        ServerBitLockerLockCommand,
 
 		/// <summary>
 		/// 클라이언트에서 서버의 명령 실행 여부에 대한 응답을 나타냅니다.
@@ -72,23 +47,53 @@ namespace IBLVM_Library.Enums
         ClientBitLockerCommandResponse,
 
 		/// <summary>
-		/// 서버에서 클라이언트의 드라이브 목록 요청을 나타냅니다.
-		/// </summary>
-        ServerDrivesRequest,
-
-		/// <summary>
 		/// 클라이언트에서 서버의 드라이브 목록 요청에 대한 응답을 나타냅니다.
 		/// </summary>
-        ClientDrivesResponse,
+		ClientDrivesResponse,
 
 		/// <summary>
-		/// 클라이언트에서 서버에 디바이스 목록 요청을 나타냅니다.
+		/// 핸드셰이크 과정에서 서버측에서 클라이언트에게 보내는 키 응답입니다.
 		/// </summary>
-        ClientDevicesRequest,
+		ServerKeyResponse,
+
+		/// <summary>
+		/// 서버에서 클라이언트 측에 로그인 결과를 알림을 나타냅니다.
+		/// </summary>
+		ServerLoginResponse,
+
+		/// <summary>
+		/// 서버에서 클라이언트에게 BitLocker 볼륨 목록에 대한 요청을 나타냅니다.
+		/// </summary>
+		ServerBitLockersRequest,
+
+		/// <summary>
+		/// 서버에서 클라이언트의 BitLocker 볼륨의 잠금 해제 명렁을 나타냅니다.
+		/// </summary>
+		ServerBitLockerUnlockCommand,
+
+		/// <summary>
+		/// 서버에서 클라이언트의 BitLocker 볼륨의 잠금 명령을 나타냅니다.
+		/// </summary>
+		ServerBitLockerLockCommand,
+
+		/// <summary>
+		/// 서버에서 클라이언트의 드라이브 목록 요청을 나타냅니다.
+		/// </summary>
+		ServerDrivesRequest,
 
 		/// <summary>
 		/// 서버에서 클라이언트의 디바이스 목록 요청에 대한 응답을 나타냅니다.
 		/// </summary>
-        ServerDevicesResponse
-    }
+        ServerDevicesResponse,
+
+		/// <summary>
+		/// 메니저에서 서버에 디바이스 목록 요청을 나타냅니다.
+		/// </summary>
+		ManagerDevicesRequest,
+
+		/// <summary>
+		/// 메니저에서 클라이언트의 드라이브 목록 요청을 나타납니다.
+		/// </summary>
+		ManagerDrivesRequest
+	}
 }
