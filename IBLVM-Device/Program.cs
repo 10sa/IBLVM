ea1c519d83db994e619ec8e8d855917790919c53
10sa <zenkins26@gmail.com>
2019-06-25 09:45:37 +0900

    Create Device console.

---
+++ IBLVM-Device/Program.cs
@@ -0,0 +1,49 @@
+﻿using IBLVM_Client;
+using IBLVM_Library.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBLVM_Device
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				try
+				{
+					IBLVMClient client = new IBLVMClient();
+					Console.WriteLine("Please enter server address.");
+					client.Connect(new IPEndPoint(IPAddress.Parse(Console.ReadLine()), 54541));
+					while (client.Status != (int)ClientSocketStatus.Connected) ;
+
+					Login(client);
+					while (client.Status != (int)ClientSocketStatus.LoggedIn) ;
+				}
+				catch (Exception)
+				{
+					continue;
+				}
+			}
+		}
+
+		private static void Login(IBLVMClient client)
+		{
+			Console.WriteLine("Connected!");
+			Console.WriteLine("Please login to server.");
+			Console.Write("ID :");
+			string id = Console.ReadLine();
+			Console.Write("Password : ");
+			string password = Console.ReadLine();
+
+
+			client.Login(id, password);
+			while (client.Status != (int)ClientSocketStatus.LoggedIn) ;
+		}
+	}
+}
