a9eaec13a9fadc1f272de5972561348cfe43844c
10sa <zenkins26@gmail.com>
2019-06-25 09:54:31 +0900

    Add Server Console.

---
+++ IBLVM-Server-Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using IBLVM_Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBLVM_Server_Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IBLVMServer server = new IBLVMServer(new SessionValidateor());
+			server.Bind(new IPEndPoint(IPAddress.Any, 54541));
+			server.Listen(5);
+			server.Start();
+
+			server.ServerThread.Join();
+		}
+	}
+}
---
+++ IBLVM-Server-Console/SessionValidateor.cs
@@ -0,0 +1,21 @@
+﻿using IBLVM_Library.Interfaces;
+using IBLVM_Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBLVM_Server_Console
+{
+	class SessionValidateor : ISession
+	{
+		public IAccount Account { get; private set; }
+
+		public bool Auth(IAccount account)
+		{
+			Account = account;
+			return true;
+		}
+	}
+}
