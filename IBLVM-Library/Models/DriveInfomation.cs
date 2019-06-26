e9fb2b91c6aaa2b26b1185785e9bb3cf6e171a15
10sa <zenkins26@gmail.com>
2019-06-26 10:14:57 +0900

    Fix Clientside listing bug.

--- IBLVM-Library/Models/DriveInfomation.cs
+++ IBLVM-Library/Models/DriveInfomation.cs
@@ -41,12 +41,18 @@ namespace IBLVM_Library.Models
 			}
 		}
 
-		public override string ToString() => $"{Name},{VolumeLabel},{TotalSize},{TotalFreeSpace},{(byte)DriveType},{IsBitLocker},{IsProtected}";
+		public DriveInformation(string name, string volumeLabel, long totalSize, long totalFreeSpace, DriveType driveType, bool isBitLocker, bool isProtected) : this(name, volumeLabel, totalSize, totalFreeSpace, driveType)
+		{
+			IsBitLocker = isBitLocker;
+			IsProtected = isProtected;
+		}
+
+		public override string ToString() => $"{Name},{VolumeLabel},{TotalSize},{TotalFreeSpace},{(byte)DriveType},{BitConverter.GetBytes(IsBitLocker)},{BitConverter.GetBytes(IsProtected)}";
 
 		public static DriveInformation FromString(string str)
 		{
 			string[] data = str.Split(',');
-			return new DriveInformation(data[0], data[1], long.Parse(data[2]), long.Parse(data[3]), (DriveType)byte.Parse(data[4]));
+			return new DriveInformation(data[0], data[1], long.Parse(data[2]), long.Parse(data[3]), (DriveType)byte.Parse(data[4]), Convert.ToBoolean(byte.Parse(data[5])), Convert.ToBoolean(byte.Parse(data[6])));
 		}
 
 		public static explicit operator DriveInformation(DriveInfo param)
